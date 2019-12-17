2317fcabe2b969e6256953d5a32f27e1722d85ec
sspork <sporksa.student@waldorfmaui.org>
2019-12-17 13:37:44 -1000

    new courses

---
+++ printing.ts
@@ -0,0 +1,11 @@
+console.log("fantasma " + "is " + "buena")
+console.log(5 + "9")
+let variable: number = 8
+console.log (5 + variable)
+if (variable == 8){
+    console.log ("variable is equal to 8")
+}else if (variable == 7){
+    console.log ("variable is equal to 7")
+}else{
+    console.log ("variable is unknown")
+}
